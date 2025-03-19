6ef1a69b20eeab141b941053edbb6a73700d82f1
unknown <104113410+dtranow@users.noreply.github.com>
2025-03-19 04:25:42 -0500

    more updates to ui and backend mainly the sidebar

--- auth-frontend/src/components/Sidebar.tsx
+++ auth-frontend/src/components/Sidebar.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
 import { VscArrowRight, VscArrowLeft } from 'react-icons/vsc'
+import axios from 'axios'
+import SidebarDeckList from './SidebarDeckList'
 import '../Dashboard.css'
 
 interface props {
@@ -8,7 +10,37 @@ interface props {
     handleLogout: () => void;
 }
 
+interface Deck {
+  id: string;
+  name: string;
+  progress: number;
+}
+
 const Sidebar: React.FC<props> = ({ isOpen, toggleSidebar, handleLogout }) => {
+  const [decks, setDecks] = useState<Deck[]>([])
+  const jwt = localStorage.getItem('token')
+
+  const fetchDecks = async () => {
+    try {
+      const res = await axios.get("/api/decks", {
+        headers: { Authorization: `Bearer ${jwt}`}
+      })
+      if(Array.isArray(res.data)){
+        setDecks(res.data)
+      }
+      else{
+        setDecks([])
+      }
+    }
+    catch(error) {
+      console.error("Failed to fetch decks", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchDecks()
+  }, [])
+  
   return (
       <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
         <button className='close-btn' onClick={toggleSidebar}>{isOpen ? <VscArrowLeft/> : <VscArrowRight/>}</button>
@@ -17,10 +49,7 @@ const Sidebar: React.FC<props> = ({ isOpen, toggleSidebar, handleLogout }) => {
           <p>_ flashcards created!</p>
         </div>
         <nav className='flashcard-list'>
-          <ul>
-            <li>flashcards deck 1</li>
-            <li>flashcards deck 2</li>
-          </ul>
+          <SidebarDeckList decks={decks || []} />
         </nav>
         <button className='log-out' onClick={handleLogout}>Logout</button>
       </div>  
--- auth-frontend/src/components/SidebarDeckList.tsx
+++ auth-frontend/src/components/SidebarDeckList.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
-import { LinearProgress, Typography, Box, Paper } from '@mui/material'
+import { LinearProgress } from '@mui/material'
+import { Link } from 'react-router-dom'
 
-const SidebarDeckList: React.FC = () => {
+interface Deck {
+  id: string;
+  name: string;
+  progress: number;
+}
+
+interface props {
+  decks: Deck[];
+}
+
+const SidebarDeckList: React.FC<props> = ({ decks }) => {
   return (
     <div>
-
+      {decks.length === 0 ? (
+        <p>No decks available</p>
+      ) : (
+        decks.map((deck, index) => (
+          <Link to={`/studycards/${deck.id}`}>
+            <div key={deck.id}>
+              <p>{deck.name}</p>
+              <LinearProgress variant='determinate' value={deck.progress}></LinearProgress>
+            </div>
+          </Link>
+        ))
+      )}
     </div>
   )
 }
